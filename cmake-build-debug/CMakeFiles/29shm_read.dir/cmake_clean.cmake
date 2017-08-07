5f08ba1fce896f68294af628088c0c017f1248d7
czw421525351 <421525351@qq.com>
2017-08-07 21:28:29 +0800

    add shm

---
+++ cmake-build-debug/CMakeFiles/29shm_read.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/29shm_read.dir/29shm_read.cpp.o"
+  "29shm_read.pdb"
+  "29shm_read.exe"
+  "lib29shm_read.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/29shm_read.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/29shm_write.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/29shm_write.dir/29shm_write.cpp.o"
+  "29shm_write.pdb"
+  "29shm_write.exe"
+  "lib29shm_write.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/29shm_write.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
