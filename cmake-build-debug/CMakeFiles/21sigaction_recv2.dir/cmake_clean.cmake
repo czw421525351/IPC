fa1ca45e2a96a67a8b832d5272f98f44a223bea0
czw421525351 <421525351@qq.com>
2017-08-07 22:20:51 +0800

    add sigaction examples

---
+++ cmake-build-debug/CMakeFiles/21sigaction_recv2.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/21sigaction_recv2.dir/21sigaction_recv2.cpp.o"
+  "21sigaction_recv2.pdb"
+  "21sigaction_recv2.exe"
+  "lib21sigaction_recv2.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/21sigaction_recv2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/21sigaction_send2.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/21sigaction_send2.dir/21sigaction_send2.cpp.o"
+  "21sigaction_send2.pdb"
+  "21sigaction_send2.exe"
+  "lib21sigaction_send2.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/21sigaction_send2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
